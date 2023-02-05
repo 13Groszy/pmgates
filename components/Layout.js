9440150bcd7840de47cc542dbcdfa09e61d31eb1
Mateusz Daszkiewicz <mateusz@wordshop.co.uk>
2023-02-05 15:38:18 +0000

    Cookies created

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,23 @@
-import Cookie from "./Cookie";
+
 import Footer from "./Footer"
 import Navbar from "./Navbar"
+import CookieConsent from "react-cookie-consent";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
   return (
     <div className="content">
       <Navbar />
       { children }
-      <Cookie />
       <Footer />
+      <CookieConsent
+        location="bottom"
+        expires={365}
+        style={{ background: "#2B373B", textAlign: "center", color: "white" }}
+        buttonStyle={{ color: "#000", fontSize: "16px", backgroundColor: "#fff", borderRadius: "5px" }}
+        >
+          By using this website you agree with our <Link href="/tos">Terms and conditions</Link>. Please accept these first.
+        </CookieConsent>
     </div>
   );
 }
