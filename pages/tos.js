3dae328d00b910ac20fcf479b73da0a112c3316f
Mateusz Daszkiewicz <mateusz@wordshop.co.uk>
2023-02-05 16:16:14 +0000

    updated TOS

--- pages/tos.js
+++ pages/tos.js
@@ -1,12 +1,19 @@
 const Tos = () => {
     return ( 
         <main>
-            <h1>Terms of Service</h1>
-            <p>Explanation of cookies + how we use them</p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa ducimus ratione rem illo assumenda provident expedita, modi quaerat corporis repudiandae perspiciatis nam maiores error a rerum. Officiis a incidunt molestiae distinctio eum, sit maiores ullam, similique animi voluptate aut porro dolore nemo suscipit. Rem, corporis rerum minima vitae itaque in.</p>
+            <div className="TOS">
+                <h2>Terms and Conditions of Use</h2>
+                <p>Welcome to our website. By using our site, you agree to these Terms and Conditions of Use (“Terms”). Please read these Terms carefully before using our site. If you do not agree to these Terms, you may not use our site.</p>
 
+                <h2>Cookie Policy</h2>
+                <p>Our website uses necessary cookies that expire after one year. These cookies are essential for the proper functioning of our site and do not collect or store any personal information. By using our site, you consent to our use of these cookies in accordance with our cookie policy.</p>
+
+                <h2>Changes to Terms</h2>
+                <p>If you have any questions about these Terms or our site, please contact us at <a href="mailto:sales@pmgates.co.uk">sales@pmgates.co.uk</a></p>
+                <p>These Terms are effective as of 01.01.2023</p>
+            </div>
         </main>
      );
 }
- 
+
 export default Tos;
